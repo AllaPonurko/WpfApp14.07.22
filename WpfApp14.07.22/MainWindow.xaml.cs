6d51f0c02eeb18aa5288afeed1f706cb0b1822c8
Alla Ponurko <ponurkoalla264@gmail.com>
2022-07-15 01:34:48 +0300

    Добавьте файлы проекта.

---
+++ WpfApp14.07.22/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp14._07._22
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Button btnMain = new Button();
+            //btnMain.Width = 200;
+            //btnMain.Height = 80;
+            //btnMain.Content = "Кнопка";
+            //layoutGrid.Children.Add(btnMain);
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
