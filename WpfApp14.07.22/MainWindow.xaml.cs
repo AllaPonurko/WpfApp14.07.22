3cf2fb6ba3d6172fd55b8919b22aecf28e8142db
Alla Ponurko <ponurkoalla264@gmail.com>
2022-07-16 01:56:31 +0300

    finish

--- WpfApp14.07.22/MainWindow.xaml.cs
+++ WpfApp14.07.22/MainWindow.xaml.cs
@@ -34,5 +34,233 @@ namespace WpfApp14._07._22
         {
             
         }
+
+        
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {if(txtEnter.Text.StartsWith("0"))
+            {
+                txtEnter.Text=txtEnter.Text.TrimStart('0');
+            }
+        if(txtEnter.Text.StartsWith("0,"))
+            txtEnter.Text += btn2.Content.ToString();
+         else
+                txtEnter.Text += btn2.Content.ToString();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEnter.Text.StartsWith("0"))
+            {
+                txtEnter.Text = txtEnter.Text.TrimStart('0');
+            }
+            if (txtEnter.Text.StartsWith("0,"))
+                txtEnter.Text += btn3.Content.ToString();
+            else
+                txtEnter.Text += btn3.Content.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            if (txtEnter.Text.StartsWith("0"))
+            {
+                txtEnter.Text = txtEnter.Text.TrimStart('0');
+            }
+            if (txtEnter.Text.StartsWith("0,"))
+                txtEnter.Text += btn1.Content.ToString();
+            else
+                txtEnter.Text += btn1.Content.ToString();
+
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEnter.Text.StartsWith("0"))
+            {
+                txtEnter.Text = txtEnter.Text.TrimStart('0');
+            }
+            if (txtEnter.Text.StartsWith("0,"))
+                txtEnter.Text += btn4.Content.ToString();
+            else
+                txtEnter.Text += btn4.Content.ToString();
+        }
+        bool summ = false;
+        bool subtraction = false;
+        bool multiply = false;
+        bool division = false;
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        double result=0;
+        private void btn_sum_Click(object sender, RoutedEventArgs e)
+        {
+            result = Convert.ToDouble(txtEnter.Text);
+            summ = true;
+            subtraction = false;
+            multiply = false;
+            division = false;
+            txtEnter.Text = null;
+        }
+
+        private void btn_result_Click(object sender, RoutedEventArgs e)
+        {
+            if (summ == true) 
+            { 
+                result += Convert.ToDouble(txtEnter.Text);
+                txtResult.Text = result.ToString();
+            }
+            if (subtraction == true)
+            {
+                result -= Convert.ToDouble(txtEnter.Text);
+                txtResult.Text = result.ToString();
+            }
+            if (multiply == true)
+            {
+                result *= Convert.ToDouble(txtEnter.Text);
+                txtResult.Text = result.ToString();
+            }
+            if (division == true && Convert.ToDouble(txtEnter.Text) == 0)
+            { 
+                MessageBox.Show("Ошибка! Деление на ноль!!!");
+                txtResult.Text = "Error";
+            }
+            if (division == true && Convert.ToDouble(txtEnter.Text) != 0)
+            { 
+                result /= Convert.ToDouble(txtEnter.Text);
+                txtResult.Text = result.ToString();
+            }
+            
+        }
+
+        private void btn_substruction_Click(object sender, RoutedEventArgs e)
+        {
+            result = Convert.ToDouble(txtEnter.Text);
+            summ = false;
+            subtraction = true;
+            multiply = false;
+            division = false;
+            txtEnter.Text = null;
+        }
+
+        private void btn_multiply_Click(object sender, RoutedEventArgs e)
+        {
+            result = Convert.ToDouble(txtEnter.Text);
+            summ = false;
+            subtraction = false;
+            multiply = true;
+            division = false;
+            txtEnter.Text = null;
+        }
+
+        private void btn_division_Click(object sender, RoutedEventArgs e)
+        {
+            result = Convert.ToDouble(txtEnter.Text);
+            summ = false;
+            subtraction = false;
+            multiply = false;
+            division = true;
+            txtEnter.Text = null;
+        }
+
+        private void btnCE_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEnter.Text != null) txtEnter.Text = null;
+        }
+
+        private void btnC_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEnter.Text != null) txtEnter.Text = null;
+            if (txtResult.Text != null) txtResult.Text = null;
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            
+            txtEnter.Text += btn0.Content.ToString();
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEnter.Text.StartsWith("0"))
+            {
+                txtEnter.Text = txtEnter.Text.TrimStart('0');
+            }
+            if (txtEnter.Text.StartsWith("0,"))
+                txtEnter.Text += btn5.Content.ToString();
+            else
+                txtEnter.Text += btn5.Content.ToString();
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEnter.Text.StartsWith("0"))
+            {
+                txtEnter.Text = txtEnter.Text.TrimStart('0');
+            }
+            if (txtEnter.Text.StartsWith("0,"))
+                txtEnter.Text += btn6.Content.ToString();
+            else
+                txtEnter.Text += btn6.Content.ToString();
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEnter.Text.StartsWith("0"))
+            {
+                txtEnter.Text = txtEnter.Text.TrimStart('0');
+            }
+            if (txtEnter.Text.StartsWith("0,"))
+                txtEnter.Text += btn7.Content.ToString();
+            else
+                txtEnter.Text += btn7.Content.ToString();
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEnter.Text.StartsWith("0"))
+            {
+                txtEnter.Text = txtEnter.Text.TrimStart('0');
+            }
+            if (txtEnter.Text.StartsWith("0,"))
+                txtEnter.Text += btn8.Content.ToString();
+            else
+                txtEnter.Text += btn8.Content.ToString();
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEnter.Text.StartsWith("0"))
+            {
+                txtEnter.Text = txtEnter.Text.TrimStart('0');
+            }
+            if (txtEnter.Text.StartsWith("0,"))
+                txtEnter.Text += btn9.Content.ToString();
+            else
+                txtEnter.Text += btn9.Content.ToString();
+        }
+
+        private void btn__Click(object sender, RoutedEventArgs e)
+        {
+            if(txtEnter.Text.Length != 0 && txtEnter.Text.Contains(","))
+            {
+                MessageBox.Show("Ошибка! В десятичное число нельзя вводить ещё один разделитель!");
+
+            }
+            else 
+            { 
+            if (txtEnter.Text.Length != 0)
+            txtEnter.Text += ",".ToString();
+            if (txtEnter.Text.Length == 0)
+                txtEnter.Text += "0,".ToString();
+            }
+        }
+
+        private void btn_RemoveLast_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEnter.Text.Length != 0)
+                txtEnter.Text = txtEnter.Text.Remove(txtEnter.Text.Length - 1);
+        }
     }
 }
